abdc00b30da861162d023cd7236feacce405719f
Eric Hayes <eric@erichayes.net>
2016-08-17 14:14:51 -0700

    Clean up front_matter script

--- exe/front_matter.rb
+++ exe/front_matter.rb
@@ -2,18 +2,7 @@
 
 base_dir = ARGV[0] || File.expand_path("../../site/docs/guides", __FILE__)
 
-# puts base_dir
-
-# find md files
-md_paths = Dir["#{base_dir}/**/*.md"]
-
-# puts md_paths
-
-# read content
-# prepend "---\n---"
-# write file
-
-md_paths.each do |path|
+Dir["#{base_dir}/**/*.md"].each do |path|
   content = File.readlines(path)
 
   File.open(path, "w") do |f|
