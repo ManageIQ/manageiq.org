3cc828e955648b3e2b45270e8b5e00b0fd074605
Eric Hayes <eric@erichayes.net>
2016-08-18 09:53:39 -0700

    WIP combining docs

--- site/_plugins/miq/markdown_link_converter.rb
+++ site/_plugins/miq/markdown_link_converter.rb
@@ -3,16 +3,22 @@ require 'pry'
 module Miq
   class MdLinkGenerator < Jekyll::Generator
     def generate(site)
-      # site.collections["dev_docs"].docs.each { |doc| rewrite_links(doc) }
+      site.pages.each {|page| rewrite_links(page) }
     end
 
-    def rewrite_links(doc)
-      # doc.content = doc.content.gsub(MD_FILE, '\1.html)')
-      doc.content = Miq.md_link_to_html(doc.content)
+    def rewrite_links(page)
+      page.content = Miq.md_link_to_html(page.content)
     end
   end
 end
 
-Jekyll::Hooks.register :site, :post_read do |post|
-  binding.pry
+Jekyll::Hooks.register :site, :post_read do |site|
+  # Collect pages with titles
+  site.data['titled_pages'] = site.pages.map {|p| p unless p['title'].nil? }.compact
 end
+
+# %i(post_init pre_render post_render post_write).each do |hook|
+#   Jekyll::Hooks.register :page, hook do |page|
+#     puts hook if page.respond_to? :title
+#   end
+# end
